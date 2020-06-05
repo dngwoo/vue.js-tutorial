f85e5c132bf9af7ae3e0a79edbd5ec34814e0911
dngwoo <akfmzk84@naver.com>
2020-06-06 00:18:18 +0900

    싱글 파일 컴포넌트에서 data 속성 사용하기

--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -4,5 +4,5 @@ import App from "./App.vue";
 Vue.config.productionTip = false;
 
 new Vue({
-  render: (h) => h(App),
+  render: (h) => h(App), // App을 화면에 뿌려주는 역할, components 속성과 같은 역할
 }).$mount("#app");
