a12f5a72195be98c10ed95bbef7d38010228874c
dngwoo <akfmzk84@naver.com>
2020-06-06 01:28:06 +0900

    vue-form 셋팅

---
+++ vue-form/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <form action="">
+    <div>
+      <!-- for은 내가 어떤 태그의 레이블인지 파악하기 위해 사용 -->
+      <label for="username">id: </label>
+      <input id="username" type="text" />
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input id="password" type="password" />
+    </div>
+    <button>login</button>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
