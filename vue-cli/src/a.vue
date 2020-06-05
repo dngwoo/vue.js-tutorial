21d292ce9f5ecad4abcdaa2a8cff30f044d0789e
dngwoo <akfmzk84@naver.com>
2020-06-05 15:45:40 +0900

    a.vue 파일 생성 및 비교

---
+++ vue-cli/src/a.vue
@@ -0,0 +1,21 @@
+var appHeader = {
+    template: '<div>Fuck</div>',
+    methods: {
+        addNum : function() {}
+    }
+}
+
+<template>
+  <div>Fuck</div>
+</template>
+
+<script>
+export default {
+  methods: {
+    addNum: function() {}
+  }
+};
+</script>
+
+<style>
+</style>
